eee41c05ae0511c9991fad00e6cee1afa6856e40
iantheninja <munene.ian@live.com>
2015-09-18 18:05:07 +0300

    adds_n toy challenge added

---
+++ adds_n/adds_n.java
@@ -0,0 +1,27 @@
+//Using Java, have the function adds_n(num) add up all the numbers from 1 to num.
+// num is a number entered in by a user(you) when prompted from the terminal 
+//For the test cases, the parameter num will be any number from 1 to 1000. 
+
+
+import java.util.*; 
+import java.io.*;
+
+class Function {  
+  int adds_n(int num) { 
+       
+    return num;
+    
+  } 
+  
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner  s = new Scanner(System.in);
+    Function c = new Function();
+    System.out.print(c.adds_n(s.nextLine())); 
+  }   
+  
+}
+
+
+
+
