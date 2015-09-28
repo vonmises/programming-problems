0e8418e0eb90e8d50d1588e095fdf53e3b222a01
vonmises <vonmises@users.noreply.github.com>
2015-09-28 15:19:13 +0300

    solved parenthesis matcher problem

--- paren_matcher/paren_matcher.py
+++ paren_matcher/paren_matcher.py
@@ -5,3 +5,24 @@
 # Not balanced:
 #   '((()'
 #   '())('
+
+def is_paren_balanced(str):
+    balanced = True
+    index = 0
+    stack = []
+
+    while index < len(str) and balanced:
+        char = str[index]
+        if char == "(":
+            stack.append(char)
+        else:
+            if len(stack) == 0:
+                balanced = False
+            else:
+                stack.pop()
+
+        index += 1
+
+    return balanced and len(stack) == 0
+
+print(is_paren_balanced('((()))'))
