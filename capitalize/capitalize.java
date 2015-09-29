f347e7377ef4164b748122e31ee0426e6876fe2d
iantheninja <munene.ian@live.com>
2015-09-29 17:39:36 +0300

    capitalize python toy problem added

--- capitalize/capitalize.java
+++ capitalize/capitalize.java
@@ -1,3 +1,4 @@
-// Using ruby, have the function capitalize(str) take the str parameter being passed and capitalize
+// Using java, have the function capitalize(str) take the str parameter being passed and capitalize
 // the first letter of each word. Words will be separated by only one space.
+// use the main method for testing and call the function from the main method
 // it should take string input from a user
