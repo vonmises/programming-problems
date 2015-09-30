7064f60a03ea02e3c592a7322c0155ea42eac34c
vonmises <vonmises@users.noreply.github.com>
2015-09-30 08:56:21 +0300

    solved the capitalise problem - python

--- capitalize_python/capitalize.py
+++ capitalize_python/capitalize.py
@@ -2,3 +2,8 @@
 # the first letter of each word. Words will be separated by only one space.
 # it should take string input from a user
 # do not use the capitalize method
+
+def capitalise(text):
+    return " ".join([word[0].upper() + word[1:] for word in text.split()])
+
+print(capitalise("some words here"))
