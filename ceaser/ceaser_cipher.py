b84945901de04a681c83487a57aa23dcb5277fcf
iantheninja <munene.ian@live.com>
2015-10-09 08:39:11 +0300

    ceaser cipher toy problem added

---
+++ ceaser/ceaser_cipher.py
@@ -0,0 +1,9 @@
+# Using the Python, have the function CaesarCipher(str,num) take the str parameter and perform a Caesar Cipher shift
+# on it using the num parameter as the shifting number.
+# A Caesar Cipher works by shifting each letter in the
+# string N places down in the alphabet (in this case N will be num).
+# Punctuation, spaces, and capitalization should remain intact.
+# For example if the string is "Caesar Cipher" and num is 2 the output should be "Ecguct Ekrjgt".
+
+# to read more about ceaser's cipher visit http://practicalcryptography.com/ciphers/caesar-cipher/ or google some more
+# happy coding :-)
