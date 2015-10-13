be0ecde89a272cfd35e01bd3f5a581aaac6586ef
iantheninja <munene.ian@live.com>
2015-10-13 08:55:04 +0300

    greatest common factor toy problem added

---
+++ GCF/GCF.java
@@ -0,0 +1,5 @@
+// Using java, have the function divide(num1,num2)
+// take both parameters being passed and return the Greatest Common Factor.
+// That is, return the greatest number that evenly goes into both numbers with no remainder.
+// For example: 20 and 10 both are divisible by 1, 2, 5 and 10 so the output should be 10.
+// The range for both parameters will be from 1 to 10^3.
