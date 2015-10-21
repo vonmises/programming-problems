4de4c3020fa0e875675b9884123ac4c1a917db29
Anthony Miyoro <amiyoro@gmail.com>
2015-10-21 08:35:49 +0300

    Added domestic press and key press solutions

--- domestic_trade/domestic_trade.py
+++ domestic_trade/domestic_trade.py
@@ -4,13 +4,16 @@
 # You have been hired by a trade company to write a program.
 
 # They have given you a CSV (comma separated value, used in spreadsheets) file containing sales data by transaction
-#for 10,000 sales transactions.
+# for 10,000 sales transactions.
 
-# Write a function that calculates the grand total of sales for a given item across all stores.
+# Write a function that calculates the grand total of sales for a given
+# item across all stores.
 
-# Your output should be in a form of a dictionary, with total_KSH as a key and the total as a value.
+# Your output should be in a form of a dictionary, with total_KSH as a key
+# and the total as a value.
 
-# Additionally, the company wants to know which store location made the largest sales for that item. Add that as another hash key-value pair.
+# Additionally, the company wants to know which store location made the largest sales for that item.
+# Add that as another hash key-value pair.
 
 # Notes:
 #  - Check out this website for an intro to handling CSV files
@@ -26,10 +29,25 @@
 #   Mombasa,DM1210,6876 KSH
 #   Nakuru,DM1182,5464 KSH
 
+
 # If we enter 'DM1182', you program should return:
 # {:total_KSH=> 13290, :largest=> 'Nairobi'}.
 
+
 def domestic_trade(itemId):
   # Your Code Here!
+    import csv
+  # Open csv file and assign it to variable opencsv
+    with open('TRANS.csv', 'r') as csvfile:
+    	total = 0
+  # Go through the csv file
+        readCSV = csv.reader(csvfile, delimiter=',')
+  # Goes through the rows in the csv file
+        for row in readCSV:
+            for field in row:
+                if field == itemId:
+           			total += str(row[2])
+           	print total
 
 domestic_trade("DM1724")
+csvfile.close()
