e1e09342b550f52c74789669b097439f8513e5ca
Anthony Miyoro <amiyoro@gmail.com>
2015-10-23 08:16:47 +0300

    Added remove duplicate problem

---
+++ remove_duplicate/remove_duplicate.py
@@ -0,0 +1,8 @@
+ # Using PYTHON solve the problem below:
+
+ # Remove duplicate characters in a given string keeping only the first occurrences. 
+ # For example, if the input is ‘tree traversal’ the output will be "tre avsl".
+
+def removeDuplicates(string):
+ 	# Your code here!
+
